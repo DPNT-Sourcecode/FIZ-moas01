f61d3ba3b4da8229616b657c039680c4c7de8a14
sbx_user1051 <sbx_user1051@169.254.1.133>
2020-09-30 12:50:48 +0000

    Wed Sep 30 12:50:48 UTC 2020

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -5,19 +5,51 @@ import befaster.runner.SolutionNotImplementedException;
 public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer n) {
-        if(n % 3 == 0 && n % 5 == 0) {
+        int fizz=0;  //1 adev, 0 fals
+        int buzz=0;
+
+        if( n % 3 == 0 ){
+            int number = n;
+            while( number > 0 ){
+                int cifra = number/10;
+                number= number/10;
+                if(cifra==3) {
+                    fizz = 1;
+                    break;
+                }
+            }
+        }
+
+        if( n % 5 == 0 ){
+            int number = n;
+            while( number > 0 ){
+                int cifra = number/10;
+                number= number/10;
+                if(cifra==5) {
+                    buzz = 1;
+                    break;
+                }
+            }
+        }
+        if( fizz == 1 && buzz == 1){
             return "fizz buzz";
-        }else if(n % 3 == 0){
+        }else if(fizz == 1){
             return "fizz";
-        }else if(n % 5 == 0){
+        }else if(buzz == 1){
             return "buzz";
         }else{
             return Integer.toString(n);
         }
+
+
+
+
+
     }
 
 
 }
+
 
 
 
