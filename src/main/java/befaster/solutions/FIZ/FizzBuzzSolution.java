0de8e93d9c23c9cf93fa94bc18bd32651253199d
sbx_user1051 <sbx_user1051@169.254.1.133>
2020-09-30 12:39:52 +0000

    Wed Sep 30 12:39:52 UTC 2020

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -7,5 +7,19 @@ public class FizzBuzzSolution {
     public String fizzBuzz(Integer number) {
         throw new SolutionNotImplementedException();
     }
+    public String fizz_buzz(Integer n){
+
+        if(n % 3 == 0 && n % 5 == 0) {
+            return "fizz buzz";
+        }else if(n % 3){
+            return "fizz";
+        }else if(n % 5){
+            return "buzz";
+        }else{
+            return Integer.toString(n);
+        }
+
+    }
 
 }
+
